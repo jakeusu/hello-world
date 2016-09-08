4689707da27919d679ebf37e10b4fd3fb478dd88
Niklas <jakeusu@gmail.com>
2016-09-08 22:13:48 +0200

    2019-9-08
    https://trello.com/c/1EZHLVLD/548-add-an-explore-section-to-top-of-quest
    ion-page

--- application/views/question_detail_draft.php
+++ application/views/question_detail_draft.php
@@ -353,20 +353,20 @@
       
 </div>
 
-<div class="row border1234 white_back padding_sm">
-  <div class="col-sm-9 col-xs-12">
-    <img class="pull-left" src="" width="70" height="70">
-    <div class="pull-left">
-      <div class="col-xs-12">
+<div class="row border1234 white_back padding_xs col-text">
+  <div class="col-sm-9 col-xs-12 canvas">
+    <img class="pull-left explore-section-image" src="<?= asset_base_url().'/images/lamp.png' ?>" width="70" height="70">
+    <div class="explore-section">
+      <h5 class="col-xs-12 BT">
         Not sure where to start? Need some question suggestions?
-      </div>
-      <div class="col-xs-12">
+      </h5>
+    </div>
+    <div class="explore-section">
         Jog, your memory and conduct a self-assessment on your business with out list of common and critical business questions. You can also browse our list of featured Relayy advisors.
-      </div>
     </div>  
   </div>
   <div class="col-sm-3 col-xs-12">
-    <button class="ob pull-right">EXPLORE QUESTIONS & EXPERTS</button>
+    <a href="http://relayy.io/explore" target="_blank"><button class="ob pull-right col-text">EXPLORE QUESTIONS & EXPERTS</button></a>
   </div>
 </div>
 
